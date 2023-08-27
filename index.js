1ea3c939b6938a3816ba4055a02b65e483b7d8d0
Juan <juanplazas28pa@gmail.com>
2023-08-27 14:16:17 -0500

    subiendo api

---
+++ index.js
@@ -0,0 +1,8 @@
+import jsonServer from "json-server";
+const server = jsonServer.create();
+const router = jsonServer.router("db.json");
+const middleware = jsonServer.defaults();
+const port = process.env.PORT || 3001;
+server.use(middleware);
+server.use(router);
+server.listen(port);
